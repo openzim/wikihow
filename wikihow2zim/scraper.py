9daa25e8a43700c10aab47efda368c33773b282c
ElenaKusevska <elena.kusevska@gmail.com>
2021-08-08 03:10:47 +0200

    Add Jinja2 templating. Edit setup.py to copy templates and assets

--- wikihow2zim/scraper.py
+++ wikihow2zim/scraper.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from .constants import URLS, getLogger
+from jinja2 import Environment, FileSystemLoader
+
+from .constants import URLS, getLogger, ROOT_DIR
 
 logger = getLogger()
 options = [
@@ -31,7 +33,20 @@ class wikihow2zim:
 
         self.url = URLS[self.language]
 
+        # jinja2 environment setup
+        self.env = Environment(
+            loader=FileSystemLoader(str(self.templates_dir)), autoescape=True
+        )
+
+    @property
+    def templates_dir(self):
+        return ROOT_DIR.joinpath("templates")
+
     def run(self):
         logger.info(
             f"running the scraper with the following arguments: \n{self.__dict__}"
         )
+
+        template = self.env.get_template("base.html")
+
+        logger.info(template.render(title="base", description="d"))
